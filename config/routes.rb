28a2b828084c2641b812f748ed7bed91c5f00cc2
Chris Nyaga <nyagachris75@gmail.com>
2017-09-13 21:45:16 +0300

    Scoping for api, versioning and resources for reminders all configured in the application router.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
   resources :home, only: :index
+  namespace :api do
+    namespace :v1 do
+      resources :reminders
+    end
+  end
   root 'home#index'
 end
