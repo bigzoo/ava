152e156a7462977d44ff9bc30a71489f6097456d
Chris Nyaga <nyagachris75@gmail.com>
2017-09-13 21:47:08 +0300

    Application controller updated with a before action to initialize a token for every user session and save the token within the session and also set is a cookie.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :sessionise
+  private
+  def sessionise
+    session[:token] = Session.create(token:rand(36**50).to_s(36)).token if !session[:token]
+    cookies.delete :token
+    cookies[:token] = session[:token]
+  end
 end
