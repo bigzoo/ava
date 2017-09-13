a1e08a7fe29a6ced7a2611552c2e50e93120ec46
Chris Nyaga <nyagachris75@gmail.com>
2017-09-13 21:46:06 +0300

    Updated router.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
   resources :home, only: :index
-  namespace :api do
-    namespace :v1 do
+  scope :api do
+    scope :v1 do
       resources :reminders
     end
   end
