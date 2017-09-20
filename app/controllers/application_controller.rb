922a221fdc924eb4332d0281c174b82f3307838c
Chris Nyaga <nyagachris75@gmail.com>
2017-09-20 15:41:00 +0300

    Method for generating tokens updated to reset invalid tokens.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :sessionise
   private
   def sessionise
-    session[:token] = Session.create(token:rand(36**50).to_s(36)).token if !session[:token]
-    cookies.delete :token
+    session[:token] = Session.create(token:rand(36**50).to_s(36)).token if !session[:token] || !Session.exists?(token:session[:token])
     cookies[:token] = session[:token]
   end
 end
