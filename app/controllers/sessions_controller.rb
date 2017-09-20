40b98b7b1cdc2ab80883b4547d187c4a17cf3470
Chris Nyaga <nyagachris75@gmail.com>
2017-09-20 17:26:00 +0300

    Updated to automatically assign someones name when they sign in with their google account.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,13 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
+    Session.find_by_token(session[:token]).update(name:user.name.split(' ')[0])
     redirect_to root_path
   end
 
   def destroy
     session[:user_id] = nil
+    Session.find_by_token(session[:token]).update(name:'')
     redirect_to root_path
   end
 end
