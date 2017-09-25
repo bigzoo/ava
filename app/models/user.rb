9a045d6d708c360ea1f525b12adf6a84f4bb3872
Chris Nyaga <nyagachris75@gmail.com>
2017-09-25 15:49:08 +0300

    User model updated to save refresh tokens for users when they log in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
       user.uid = auth.uid
       user.name = auth.info.name
       user.oauth_token = auth.credentials.token
+      user.refresh_token = auth.credentials.refresh_token if auth.credentials.refresh_token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
     end
