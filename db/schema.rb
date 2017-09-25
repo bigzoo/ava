4b70d30eacaa83ebed9947274b126a3cfb5577ea
Chris Nyaga <nyagachris75@gmail.com>
2017-09-25 15:50:03 +0300

    Migration to create the refresh token field created and resultant schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920081014) do
+ActiveRecord::Schema.define(version: 20170922074741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170920081014) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "refresh_token"
   end
 
 end
